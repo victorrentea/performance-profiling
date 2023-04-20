ba83ac0bfeec60bd107a8baee3d98d92f1fea591
Victor Rentea <victorrentea@gmail.com>
2023-04-20 11:39:57 +0300

    DO NOT PUT @Transactional on READ-only methods!

--- main/src/main/java/victor/training/performance/profiling/LoanService.java
+++ main/src/main/java/victor/training/performance/profiling/LoanService.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class LoanService {
   private final LoanApplicationRepo loanApplicationRepo;
@@ -30,6 +29,7 @@ public class LoanService {
 
   private final AuditRepo auditRepo;
 
+  @Transactional
   public void saveLoanApplication(String title) {
     Long id = loanApplicationRepo.save(new LoanApplication().setTitle(title)).getId();
     auditRepo.save(new Audit("Loan created: " + id));
