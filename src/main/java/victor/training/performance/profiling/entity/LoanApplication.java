9d8c7916b261c0c3418136f7bccde41744803008
Victor Rentea <victorrentea@gmail.com>
2024-11-04 15:06:05 +0200

    1) no more lazy loading in toString! 2) log.trace("{}",second)

--- src/main/java/victor/training/performance/profiling/entity/LoanApplication.java
+++ src/main/java/victor/training/performance/profiling/entity/LoanApplication.java
@@ -11,9 +11,11 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 @Entity
-@Data // generates @HashCodeEquals and @ToString on ALL FIELDS
-//@Getter @Setter
-public class LoanApplication {
+// ❌ NEVER USE @Data on @Entity!!!!
+//@Data // (lombok) generates @HashCodeEquals and @ToString on ALL FIELDS :
+// instead use just
+@Getter @Setter
+public class LoanApplication { // toString
   public enum Status {NOT_STARTED, PENDING, APPROVED, DECLINED}
 
   @Id
@@ -23,6 +25,14 @@ public class LoanApplication {
   private List<ApprovalStep> steps = new ArrayList<>();
   @ManyToMany
   private List<LoanClient> beneficiaries = new ArrayList<>();
+
+  @Override
+  public String toString() {
+    return "LoanApplication{" +
+           "id=" + id +
+           ", title='" + title + '\'' +
+           '}';
+  }
 
   public Status getCurrentStatus() {
     return getLastStep().getStatus();
