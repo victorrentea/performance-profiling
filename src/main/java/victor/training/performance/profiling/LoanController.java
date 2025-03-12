f034d75ac9b56c827777087aa96391753ff17e0c
Victor Rentea <victorrentea@gmail.com>
2025-03-12 09:39:30 +0200

    yolo

--- src/main/java/victor/training/performance/profiling/LoanController.java
+++ src/main/java/victor/training/performance/profiling/LoanController.java
@@ -1,11 +1,15 @@
 package victor.training.performance.profiling;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.distribution.HistogramSupport;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import victor.training.performance.profiling.dto.LoanApplicationDto;
 import victor.training.performance.profiling.entity.LoanApplication;
 
+import java.time.Duration;
 import java.util.List;
 
 @Slf4j
@@ -13,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class LoanController {
   private final LoanService loanService;
+  private final MeterRegistry meterRegistry;
 
   @GetMapping("loan/{id}")
   public LoanApplicationDto get(@PathVariable Long id) {
@@ -21,12 +26,18 @@ public class LoanController {
 
   @PostMapping("loan/{title}")
   public void save(@PathVariable String title) {
+    meterRegistry.counter("loans_created").increment();
     loanService.saveLoanApplication(title);
   }
 
   @GetMapping("loan/{id}/status")
   public LoanApplication.Status getStatus(@PathVariable Long id) {
-    return loanService.getLoanStatus(id);
+    Timer timer = Timer.builder("get_loan_status")
+        .publishPercentiles(0.5, 0.9, 0.99) // collect percentiles like median, p90, p99
+        .publishPercentileHistogram(true)
+        .sla(Duration.ofMillis(100), Duration.ofMillis(500), Duration.ofMillis(1000)) // SLA Buckets
+        .register(meterRegistry);
+    return timer.record(() -> loanService.getLoanStatus(id));
   }
 
   @GetMapping("loan/recent")
