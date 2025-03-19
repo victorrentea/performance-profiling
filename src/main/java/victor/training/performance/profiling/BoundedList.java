50f38815774b9a9c8d2f7259ecb405e1aa92ef5d
Victor Rentea <victorrentea@gmail.com>
2025-03-19 15:20:49 +0200

    new Object() to scare people

--- src/main/java/victor/training/performance/profiling/BoundedList.java
+++ src/main/java/victor/training/performance/profiling/BoundedList.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class BoundedList {
   private final List<Long> list = new ArrayList<>();
+  private static final Object lock = new Object();
   private final int maxSize;
 
   public BoundedList(int maxSize) {
@@ -12,14 +13,14 @@ public class BoundedList {
   }
 
   public void add(Long loanId) {
-    synchronized (list) {
+    synchronized (lock) {
       list.remove(loanId); // BUG#7235 - avoid duplicates in list
       list.add(loanId);
       while (list.size() > maxSize) list.remove(0);
     }
   }
   public ArrayList<Long> getACopy() {
-    synchronized (list) {
+    synchronized (lock) {
       return new ArrayList<>(list);
     }
   }
