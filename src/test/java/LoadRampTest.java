f7ddcd90e63b94b6afa49a3a3a6765b7fa9674a3
Victor Rentea <victorrentea@gmail.com>
2025-05-13 12:23:11 +0300

    🍒

--- src/test/java/LoadRampTest.java
+++ src/test/java/LoadRampTest.java
@@ -11,6 +11,13 @@ public class LoadRampTest extends Simulation {
     setUp(scenario(getClass().getSimpleName()).exec(http("")
             .get("/loan/1"))
         .injectClosed(rampConcurrentUsers(0).to(30).during(15)))
+        // For searching for the breaking point:
+        // on what load do I start seeing errors or exceed my SLA
+        // - you can throttle (ratelimit) your own ingress/pod to stay below the breaking point
+        //    a) global throttling per my instance =>503 Service unavailable
+        //    b) Fairness: throttle / client id => 429 Too many request = you track client JWT/Basic [HARD]
+        // - request more hardware / how many pods you might need in prod
+        // ⚠️ it's idealized. no other endpoint is called at the same time.
 
         .protocols(http.baseUrl(host));
   }
