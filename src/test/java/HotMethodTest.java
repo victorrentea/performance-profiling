e48b0cdadfebabbce8b1d801e501c04f7d8b53e5
Victor Rentea <victorrentea@gmail.com>
2025-05-13 15:35:38 +0300

    yolo

--- src/test/java/HotMethodTest.java
+++ src/test/java/HotMethodTest.java
@@ -1,20 +1,22 @@
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
 
+import static java.lang.System.currentTimeMillis;
 import static java.util.stream.Collectors.toSet;
 
 // in real life, microbenchmark using  Java Measuring Harness (JMH)
+@Slf4j
 public class HotMethodTest {
   @Test
   void fast() {
-    Set<Integer> hashSet = IntStream.range(0, 100_000).boxed()
-        .collect(toSet()); // returns a HashSet
-    List<Integer> list = IntStream.range(0, 99_999).boxed().toList();
-
-    hashSet.removeAll(list);
+    Set<Integer> hashSet = IntStream.range(0, 100_000).boxed().collect(toSet()); // returns a HashSet
+    List<Integer> list = IntStream.range(0, 99_999+2).boxed().toList();
+    hashSet.removeAll(list); // 60 ms -> 10.000 ms
+    // CPU hotspot;
   }
 
   @Test
