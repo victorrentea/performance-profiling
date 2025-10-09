03107d1626d1f823c33a7635f3c57202d94c6ebd
Victor Rentea <victorrentea@gmail.com>
2025-10-09 15:42:26 +0300

    Revert "upgrade spring boot + java ver"
    This reverts commit 416e75aa520f48bfb638c8c57605a5d5719eb8cb.

--- src/test/java/LoadTest.java
+++ src/test/java/LoadTest.java
@@ -12,30 +12,25 @@ public class LoadTest extends Simulation {
     GatlingEngine.startClass(LoadTest.class);
   }
 
+  // spike test = push as much load to take your system down.
+  // Question: did I loose any data after a 200 to client?
+  // Example #1: the app accepts a request and enqueues it in
+  // Example #2: listener acks 1..1000(page) message to broker before processing it, then processing crashes
 
   {
     setUp(scenario(getClass().getSimpleName())
         .exec(http("").get("/loan/1"))
-        // 1) Closed World: N parallel threads firing requests in a loop
-        // = # of requests in progress at any time = actual load => easier to reason
-        .injectClosed(constantConcurrentUsers(23).during(ofSeconds(8))))
+        // 1) Closed World: 23 parallel requests at all times = real issue = easier to reason
+//        .injectClosed(constantConcurrentUsers(23).during(ofSeconds(8))))
 
-        // 2) Open World: N requests / second => closer to a www userbase
-        // the longer the latency => the higher the concurrent load
-//        .injectOpen(constantUsersPerSec(200f).during(ofSeconds(8))))
+        // 2) Open World: 200 requests / second = closer to real world;
+        .injectOpen(constantUsersPerSec(200f).during(ofSeconds(8))))
         // To convert ~= closedworld.threads/endpoint_latency = 23/100ms = 230rps (eg)
 
         .protocols(http.baseUrl("http://localhost:8080"))
-
-        // ⚠️ Check all requests were successful! TRAP: Error requests tend to be very fast.
+        // ⚠️ Fast-Errors: Check all requests were successful!
         .assertions(global().successfulRequests().percent().gt(99.0));
   }
 
-  // ⚠️ Reproducible = Prove that rerunning the load test produces similar results!
-
-  // ** Spike test ** = push as much load to take your system down.
-  // Question: did I loose any data after I responded 200 OK to client?
-  // Example #1: the app accepts a request and enqueues it in memory for later
-  // Example #2: listener acks 1..1000(a page) messages to broker before processing it (which crashes)
-
+  // ⚠️ Reproducible: Prove that rerunning the load test produces similar results!
 }
