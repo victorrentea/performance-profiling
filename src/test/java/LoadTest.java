4159ba904c2f15f9fa5f14a04f2d142ecd751384
Victor Rentea <victorrentea@gmail.com>
2026-02-06 12:45:40 +0100

    polish

--- src/test/java/LoadTest.java
+++ src/test/java/LoadTest.java
@@ -6,7 +6,6 @@ import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static java.time.Duration.ofSeconds;
 
-// Gatling vs K6
 public class LoadTest extends Simulation {
   public static void main(String[] args) {
     GatlingEngine.startClass(LoadTest.class);
@@ -17,12 +16,10 @@ public class LoadTest extends Simulation {
         .exec(http("").get("/loan/1"))
         // 1) Closed World (easier): N parallel threads firing requests in a loop => max N concurrent requests at any time
         .injectClosed(constantConcurrentUsers(23).during(ofSeconds(8))))
-
         // 2) Open World: N requests / second => closer to a www userbase: higher latency => higher concurrency
         // .injectOpen(constantUsersPerSec(200f).during(ofSeconds(8))))
 
         // To convert ~= closedworld.threads/endpoint_latency = 23/100ms = 230rps (eg)
-
         .protocols(http.baseUrl("http://localhost:8080"))
 
         // ⚠️ Check all requests were successful! TRAP: Error requests tend to be very fast.
