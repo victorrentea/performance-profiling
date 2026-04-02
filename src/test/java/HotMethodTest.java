029ee7cf0b68f78057eb64fca08d9376292cf066
Victor Rentea <victorrentea@gmail.com>
2026-04-02 07:19:14 +0200

    hot flow

--- src/test/java/HotMethodTest.java
+++ src/test/java/HotMethodTest.java
@@ -1,27 +1,33 @@
+import org.junit.jupiter.api.MethodOrderer.MethodName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.List;
 import java.util.Set;
-import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toSet;
+import static java.util.stream.IntStream.range;
 
-// microbenchmark sub-ms code using Java Measuring Harness (JMH)
+// ⚠️Only microbenchmark sub-ms code using Java Measuring Harness (JMH)
+@TestMethodOrder(MethodName.class)
 public class HotMethodTest {
+  Set<Integer> hashSet = range(0, 100_000).boxed().collect(toSet());
+
   @Test
-  void fast() {
-    Set<Integer> hashSet = IntStream.range(0, 100_000).boxed()
-        .collect(toSet()); // returns a HashSet
-    List<Integer> list = IntStream.range(0, 99_999).boxed().toList();
+  void a_one() {
+    hashSet.remove(1);
+  }
+
+  @Test
+  void b_fast_99k() {
+    List<Integer> list = range(0, 99_999).boxed().toList();
 
     hashSet.removeAll(list);
   }
 
   @Test
-  void slow() {
-    Set<Integer> hashSet = IntStream.range(0, 100_000).boxed()
-        .collect(toSet()); // returns a HashSet
-    List<Integer> list = IntStream.range(0, 100_000).boxed().toList();
+  void c_slow_100k() {
+    List<Integer> list = range(0, 100_000).boxed().toList();
 
     hashSet.removeAll(list);
   }
